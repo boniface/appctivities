49a2f4a01ed0fded0efd903150c611baef6c8248
boniface <boniface@kabaso.com>
2020-06-29 17:12:59 +0200

    Update the gradle libraries for Testing

--- app/src/androidTest/java/zm/hashcode/appctivities/MainActivityTest.java
+++ app/src/androidTest/java/zm/hashcode/appctivities/MainActivityTest.java
@@ -1,35 +1,45 @@
 package zm.hashcode.appctivities;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.MediumTest;
+import androidx.test.rule.ActivityTestRule;
 
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @MediumTest
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    private String stringToBetyped;
 
-
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Hello Test";
     }
 
     @Test
-    public void test_ActivityMain() {
-        ActivityScenario<MainActivity> activityScenario = ActivityScenario.launch(MainActivity.class);
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.inputTextBox))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.textToBeChanged)).perform(click());
 
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged))
+                .check(matches(withText(stringToBetyped)));
     }
 }
--- app/src/main/java/zm/hashcode/appctivities/HelloActivity.java
+++ app/src/main/java/zm/hashcode/appctivities/HelloActivity.java
@@ -1,5 +1,7 @@
 package zm.hashcode.appctivities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,23 +11,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class HelloActivity extends AppCompatActivity {
+public class HelloActivity  extends Activity implements View.OnClickListener {
+
+    // The TextView used to display the message inside the Activity.
+    private TextView mTextView;
+
+    // The EditText where the user types the message.
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Set the listeners for the buttons.
+        findViewById(R.id.changeTextBt).setOnClickListener(this);
+        findViewById(R.id.activityChangeTextBtn).setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textToBeChanged);
+        mEditText = (EditText) findViewById(R.id.editTextUserInput);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Get the text from the EditText view.
+        final String text = mEditText.getText().toString();
+
+        final int changeTextBtId = R.id.changeTextBt;
+        final int activityChangeTextBtnId = R.id.activityChangeTextBtn;
+
+        if (view.getId() == changeTextBtId) {
+            // First button's interaction: set a text in a text view.
+            mTextView.setText(text);
+        } else if (view.getId() == activityChangeTextBtnId) {
+            // Second button's interaction: start an activity and send a message to it.
+            Intent intent = ShowTextActivity
+                    .newStartIntent(this, text);
+            startActivity(intent);
+        }
     }
 }
